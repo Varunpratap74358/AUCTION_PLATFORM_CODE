4ed54b2eac2ef5b49261040a40246ddb63ecd5e7
Varin Singh <varunpratap74358@gmail.com>
2024-10-30 06:54:03 -0700

    some change

--- Backend/routes/superAdminRoute.js
+++ Backend/routes/superAdminRoute.js
@@ -4,12 +4,12 @@ import { deleteAuctionItem, deletePaymentProof, fetchAllUsers, getPaymentProofDe
 
 export const routes = express.Router()
 
-routes.delete("/auctionitem/delete/:id",isAuthenticated,isAuthorized("Super Admin"),deleteAuctionItem)
-routes.get("/paymentproofs/getall",isAuthenticated,isAuthorized("Super Admin"),getPaymentProofs)
-routes.get("/paymentproof/:id",isAuthenticated,isAuthorized("Super Admin"),getPaymentProofDetail)
-routes.put("/paymentproof/status/update/:id",isAuthenticated,isAuthorized("Super Admin"),updateProofStatus)
-routes.delete("/paymentproof/delete/:id",isAuthenticated,isAuthorized("Super Admin"),deletePaymentProof)
-routes.get("/users/getall",isAuthenticated,isAuthorized("Super Admin"),fetchAllUsers)
-routes.get("/monthlyincome",isAuthenticated,isAuthorized("Super Admin"),monthlyRevenue)
+routes.delete("/auctionitem/delete/:id",isAuthenticated,isAuthorized("Super Admin"),deleteAuctionItem);
+routes.get("/paymentproofs/getall",isAuthenticated,isAuthorized("Super Admin"),getPaymentProofs);
+routes.get("/paymentproof/:id",isAuthenticated,isAuthorized("Super Admin"),getPaymentProofDetail);
+routes.put("/paymentproof/status/update/:id",isAuthenticated,isAuthorized("Super Admin"),updateProofStatus);
+routes.delete("/paymentproof/delete/:id",isAuthenticated,isAuthorized("Super Admin"),deletePaymentProof);
+routes.get("/users/getall",isAuthenticated,isAuthorized("Super Admin"),fetchAllUsers);
+routes.get("/monthlyincome",isAuthenticated,isAuthorized("Super Admin"),monthlyRevenue);
 
 export default routes
